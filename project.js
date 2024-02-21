44b36a1a62625a3e9cadf96c795a83c209e13db9
Jason Dogbey <apple@macbook-pro-2.home>
2024-02-21 16:38:54 +0000

    Get deposit

--- project.js
+++ project.js
@@ -5,3 +5,19 @@
 //check if user won
 //give the user their wins
 //play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    while(true){
+        const amount = prompt("How much would you like to deposit? $")
+        numberDepositAmount = parseFloat(amount);
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+            console.log("Invalid amount! Try again.")
+        } else {
+            return numberDepositAmount;
+        }
+    }
+}
+
+console.log(deposit())
