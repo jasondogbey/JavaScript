0490bd6d12d6999fea71aa1e6b4a3e1ce7470ee4
Jason Dogbey <jgdogbey@gmail.com>
2024-02-22 17:15:00 +0000

    spin slot machine and transpose matrix

--- project.js
+++ project.js
@@ -8,6 +8,23 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3
+const COLS = 3
+
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2 
+}
+
 const deposit = () => {
     while(true){
         const amount = prompt("How much would you like to deposit? $")
@@ -44,11 +61,49 @@ const getBet = (balance, lines) => {
     }
 }
 
+const spin = () => {
+    const symbols = []
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)){
+        for (let i = 0; i < count; i++){
+            symbols.push(symbol)
+        }
+    }
+    console.log(symbols)
+
+    const reels = []
+
+    for (let i = 0; i < COLS; i++){
+        reels.push([])
+        const currentSymbols = [...symbols]
+        for (let j = 0; j < ROWS; j++){
+            const randomindex = Math.floor(Math.random() * currentSymbols.length)
+            const selecetedSymbol = currentSymbols[randomindex]
+            reels[i].push(selecetedSymbol)
+            currentSymbols.splice(randomindex, 1)
+        }
+    }
+    return reels;
+}
+
+const transpose = (reels) => {
+    const rows = []
+    for (let i = 0; i < ROWS; i++){
+        rows.push([])
+        for (let j = 0; j < COLS; j++){
+            rows[i].push(reels[j][i])
+        }
+    }
+    return rows;
+}
 
 let balance = deposit()
 const numberOfLines = getNumberOfLines()
 const betPerLine = getBet(balance, numberOfLines)
+const reels = spin()
+const rows = transpose(reels)
 
+console.log(reels)
+console.log(rows)
 console.log("Balance is $" + balance)
 console.log("Number of lines is: " + numberOfLines)
 console.log("Bet per line is: " + betPerLine)
